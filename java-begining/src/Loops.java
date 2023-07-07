b347f21fda38c4a0208be5d33963347b7af03003
Faisal Javid <faisalljavid@gmail.com>
2023-07-07 22:28:31 +0530

    2nd day

--- java-begining/src/Loops.java
+++ java-begining/src/Loops.java
@@ -1,13 +1,62 @@
-public class Loops {
+import java.util.Scanner;
+public class Loops  {
     public static void main(String[] args) {
-//        int count = 1;
-//        while (count != 5) {
-//            System.out.println(count);
-//            count++;
+        /*
+            Syntax of FOR LOOP:
+
+            for (initialisation; condition; increment/decrement) {
+                // body
+            }
+         */
+
+        // Q: Print numbers from 1 to 5
+//        for (int num = 1; num <= 5; num+=1) {
+//            System.out.println(num);
 //        }
 
-        for (int count = 1; count != 5; count++) {
-            System.out.println(count);
-        }
+        // Q: Print numbers from 1 to n:
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Enter number: ");
+//        int n = in.nextInt();
+//
+//        for (int num = 1; num <= n; num++) {
+//            System.out.println(num + " ");
+//        }
+
+
+        // WHILE LOOPS (used when we don't know how many times the loop is going to run)
+
+        /*
+         Syntax:
+
+         initialisation;
+         while (condition) {
+            body
+            increment/decrement
+         }
+         */
+
+//        int num = 1;
+//        while (num <= 5) {
+//            System.out.println(num);
+//            num++; // num += 1 or 2 or 3;
+//        }
+
+        // DO WHILE LOOPS (when we want to execute the loop for at-least once)
+
+        /*
+            Syntax:
+
+            initialisation;
+            do {
+                body
+                increment/decrement
+            } while (condition);
+         */
+        int n = 1;
+        do {
+            System.out.println(n);
+            n++;
+        } while (n <= 6);
     }
 }
